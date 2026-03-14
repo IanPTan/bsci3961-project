8cd5ee9e3e4c642ae6aaa14dc8dd561d6de79aea
Ian Titor <ianpatricktan@gmail.com>
2026-03-13 20:49:59 -0400

    tested vae load and inference

---
+++ src/gen_vae_patches.py
@@ -0,0 +1,34 @@
+import torch as pt
+import torchvision.transforms as transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+
+from dataset import PatchDataset
+from vae import VAE
+
+IMAGE_PATH = 'frieren128.png'
+print(f"Using image path: {IMAGE_PATH}")
+IMG = transforms.ToTensor()(transforms.Resize(128)(Image.open(IMAGE_PATH)))
+dataset = PatchDataset(IMG, 64, 1)
+
+conv_channels = [32, 64, 128, 256, 512, 1024]
+linear_features = [128, 64]
+
+device = pt.device('cuda' if pt.cuda.is_available() else 'cpu')
+print(f"Using device: {device}")
+
+print("Loading model...")
+vae_model = VAE(conv_channels=conv_channels, linear_features=linear_features).to(device)
+vae_model.load_state_dict(pt.load("vae.pt", map_location=device))
+vae_model.eval()
+print("Loaded model.")
+
+
+for (img, _) in dataset:
+    x = img[None, ...]
+    v, _ = vae_model.encode(x)
+    y = vae_model.decode(v)
+    plt.imshow(y[0].detach().permute(1, 2, 0))
+    plt.show()
+    
